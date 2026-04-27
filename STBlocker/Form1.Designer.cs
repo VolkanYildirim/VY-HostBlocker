289238c79887e15c2d6588ccdc80e9a1828a4980
Volkan Yıldırım <23458532+VolkanYildirim@users.noreply.github.com>
2026-04-27 20:45:46 +0300

    Add project files.

---
+++ STBlocker/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace STBlocker {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 38);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(10, 0, 3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(311, 199);
+            this.listBox1.TabIndex = 5;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(245, 11);
+            this.addButton.Margin = new System.Windows.Forms.Padding(180, 0, 3, 3);
+            this.addButton.Name = "addButton";
+            this.addButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "Ekle";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(248, 275);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(3, 50, 3, 3);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 9;
+            this.deleteButton.Text = "Sil";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.checkBox1.Location = new System.Drawing.Point(12, 254);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Zamanlı";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // datePicker
+            // 
+            this.datePicker.Location = new System.Drawing.Point(12, 277);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(200, 20);
+            this.datePicker.TabIndex = 11;
+            this.datePicker.ValueChanged += new System.EventHandler(this.datePicker_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(343, 318);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.deleteButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Site Engelle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DateTimePicker datePicker;
+    }
+}
+
---
+++ STBlocker/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STBlocker {
+
+    public struct Blocked {
+        public string name;
+        public DateTime? time;
+
+        public Blocked(string name, DateTime? time) {
+            this.name = name;
+            this.time = time;
+        }
+    }
+    public partial class Form1 : Form {
+
+        private const string hostLoc = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+        private Timer timer;
+        private int timerInterval = 1000;
+
+        private List<Blocked> blockedSites;
+
+        public Form1() {
+            InitializeComponent();
+            datePicker.Visible = false;
+            datePicker.Format = DateTimePickerFormat.Time;
+            blockedSites = new List<Blocked>();
+            ReadBlocked();
+
+            timer = new Timer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = timerInterval;
+            timer.Start();
+        }
+
+        public void ReadBlocked() {
+            int count = 0;
+            using (StreamReader reader = File.OpenText(hostLoc)) {
+                string line;
+                while ((line = reader.ReadLine()) != null) {
+                    if (!String.IsNullOrEmpty(line) && !line.Contains("#")) {
+                        int index = line.IndexOf(" ");
+                        listBox1.Items.Add(line.Substring(index+1));
+                        Blocked blocked = new Blocked(line.Substring(index + 1), null);
+                        blockedSites.Add(blocked);
+                    }
+                        
+                    count++;
+                }
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e) {
+            if (String.IsNullOrEmpty(textBox1.Text))
+                return;
+
+            if (!checkBox1.Checked) {
+                string newSite = "127.0.0.1 " + textBox1.Text;
+
+                File.AppendAllText(hostLoc,
+                       newSite + Environment.NewLine);
+
+                listBox1.Items.Add(textBox1.Text);
+
+                Blocked blocked = new Blocked(textBox1.Text, null);
+                blockedSites.Add(blocked);
+
+            } else {
+                if( datePicker.Value <= DateTime.Now) {
+                    MessageBox.Show("Şu andan ileri bir tarih seçin");
+                    return;
+                }
+
+
+                Blocked blocked = new Blocked(textBox1.Text, datePicker.Value);
+                blockedSites.Add(blocked);
+
+                string newSite = textBox1.Text +" : "+ blocked.time.ToString();
+
+                File.AppendAllText(hostLoc, "127.0.0.1 " + newSite + Environment.NewLine);
+
+                listBox1.Items.Add(newSite);
+            }
+            
+        }
+
+        private void timerTick(object sender, EventArgs e) {
+            for (int i = 0; i < blockedSites.Count; i++) {
+                Blocked blocked = blockedSites[i];
+                if (blocked.time != null) {
+                    if (blocked.time < DateTime.Now) {
+                        int removed = deleteBlock(blocked.name);
+                        listBox1.Items.RemoveAt(removed);
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private int deleteBlock(string name) {
+            String[] lines = File.ReadAllLines(hostLoc);
+            int index = 0;
+            foreach (String item in lines) {
+                index++;
+                if (String.IsNullOrEmpty(item) || item.Contains("#"))
+                    continue;
+                if (item.Contains(name)) {
+                    index--;
+                    break;
+                }
+
+            }
+            var lineList = lines.ToList();
+            lineList.RemoveAt(index);
+
+            File.WriteAllLines(hostLoc, lineList.ToArray());
+
+            index = 0;
+            foreach(Blocked blocked in blockedSites) {
+                if (name.Contains(blocked.name)) {
+                    break;
+                }
+                index++;
+            }
+            //index--;
+            blockedSites.RemoveAt(index);
+
+            return index;
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e) {
+            if(listBox1.SelectedItem != null) {
+                string blockName = listBox1.SelectedItem.ToString();
+
+                int deleteIndex = deleteBlock(blockName);
+
+                listBox1.Items.RemoveAt(deleteIndex);
+            }
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            if (checkBox1.Checked)
+                datePicker.Visible = true;
+            else
+                datePicker.Visible = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void datePicker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
